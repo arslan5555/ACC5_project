434cc15f07bf434a46947b0f07f341582b83d03c
arslan5555 <arslanhameed8@gmail.com>
2020-10-22 08:53:52 +0000

    lab3 task 2

---
+++ app.py
@@ -0,0 +1,31 @@
+from collections import Counter
+from flask import jsonify
+from tasks import file_function
+import os, time
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'],)
+def counter():
+	total_count = Counter({'han':0, 'hon':0, 'den':0, 'det':0, 'denna':0, 'denne':0,'hen':0})
+	unique_tweet_count=0
+	my_list=[]
+	path = "/home/ubuntu/data"
+	dir_list = os.listdir(path)
+	start_time=time.time()
+	for name in dir_list:
+		my_list.append(file_function.delay(name))
+	for x in my_list:
+		res=x.get()
+		m=Counter(res[0])
+		unique_tweet_count=res[1]+unique_tweet_count
+		total_count=m + total_count
+	print(total_count)
+	print("Unique tweet count: ",unique_tweet_count)
+	end_time=time.time()
+	print("time is %s seconds" % (time.time() - start_time))
+	return 'Pronouns_{} Unique_Tweets_Count={}'.format(total_count,unique_tweet_count)
+if __name__ == '__main__':
+    #app.debug = True
+    app.run(host='0.0.0.0', port=5000)
